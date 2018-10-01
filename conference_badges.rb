13cfdd909c75af9170dd8c2d58a0af2ad2d114b8
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 14:59:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,8 @@
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+guestList = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
+
+def batch_badge_creator(guestList)
+  
