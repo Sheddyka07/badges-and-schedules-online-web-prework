066d17ff2d310e8a325b7e38cd0a2de399db422f
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 15:59:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,13 @@ end
 def batch_badge_creator(attendees)
   badges.each {|attendees| "Hello, my name is #{attendees}"}
 end
- # should return a list of welcome messages and room assignments
+
+
 def assign_rooms(attendees)
     room_assignments.each_with_index {|attendees, room_assignments| "Hello, #{attendees}! You'll be assigned to #{room_assignments+1}!"}
 end
- # should puts the list of badges and room_assignments
+
+
 def printer(attendees)
   batch_badge_creator(attendees).each {|badge| puts badge}
    assign_rooms(attendees).each {|room_assignment| puts room_assignment}
