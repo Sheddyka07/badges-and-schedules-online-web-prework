9939a373f205a8ed7ac7683ba932f1d65b6d9173
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 15:15:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,18 +8,5 @@ end
 
 def batch_badge_creator
   badges = guestList.map {|x| badge_maker(x)}
-  
-  
-  
-
-    
-  else
-   
-  end
 end 
   
-roomNum = [1, 2, 3, 4, 5, 6, 7] 
-
-def assign_rooms(roomNum)  
-  puts "Hello, #{name}, You'll be assigned to room #{roomNum}!"
-end 
