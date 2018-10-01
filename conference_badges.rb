f1d3e76473114952d340fc23a2555e92fcb33a7b
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 15:02:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,11 @@ end
 
 def batch_badge_creator(guestList)
   
+  badgeScript
+  
+  
+  
   
 roomNum = [1, 2, 3, 4, 5, 6, 7] 
 def assign_rooms(roomNum)  
+  puts "Hello, #{name}, You'll be assigned to room #{roomNum}!"
